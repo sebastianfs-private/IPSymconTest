29afa142a912dded9952874c4781586883e060a6
Sebastian Fasold-Schmid <sebastian@fasold-schmid.com>
2019-09-08 13:25:10 +0200

    test

--- RobonectDevice2/module.php
+++ RobonectDevice2/module.php
@@ -175,6 +175,7 @@ class RobonectDevice2 extends IPSModule
         $instances = IPS_GetInstanceListByModuleID('{51D61D4E-17D3-49C0-A07F-3E0BE775FDA5}');
         if (!$instances) {
             $this->SendDebug(__FUNCTION__, 'trigger delete variable profiles', 0);
+            IPS_DeleteVariableProfile("Robonect.Mode");
             IPS_DeleteVariableProfile("Robonect.Action");
             IPS_DeleteVariableProfile("Robonect.Activity");
             IPS_DeleteVariableProfile("Robonect.Error");
